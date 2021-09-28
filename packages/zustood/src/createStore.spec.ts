c501372277ede0c95e6bca18db0de30a8432b7bb
zbeyens <ziyad.beyens@gmail.com>
2021-09-28 16:45:47 +0200

    docs

--- packages/zustood/src/createStore.spec.ts
+++ packages/zustood/src/createStore.spec.ts
@@ -1,11 +1,80 @@
 import { createStore } from './createStore';
 
-describe('when init', () => {
-  it('should be', () => {
-    expect(
-      createStore('test')({
-        test: 1,
-      }).get.test()
-    ).toEqual(1);
+describe('zustood', () => {
+  describe('when get', () => {
+    const store = createStore('repo')({
+      name: 'zustood',
+      stars: 0,
+    });
+
+    it('should be', () => {
+      expect(store.get.name()).toEqual('zustood');
+    });
+  });
+
+  describe('when extending actions', () => {
+    const store = createStore('repo')({
+      name: 'zustood',
+      stars: 0,
+    })
+      .extendActions((set, get, api) => ({
+        validName: (name: string) => {
+          set.name(name.trim());
+        },
+      }))
+      .extendActions((set, get, api) => ({
+        reset: (name: string) => {
+          set.validName(name);
+          set.stars(0);
+        },
+      }));
+
+    it('should be', () => {
+      store.set.reset('test ');
+
+      expect(store.get.state()).toEqual({
+        name: 'test',
+        stars: 0,
+      });
+    });
+  });
+
+  describe('when extending selectors', () => {
+    const store = createStore('repo')({
+      name: 'zustood ',
+      stars: 0,
+    })
+      .extendSelectors((set, get, api) => ({
+        validName: () => get.name().trim(),
+      }))
+      .extendSelectors((set, get, api) => ({
+        title: (prefix: string) =>
+          `${prefix + get.validName()} with ${get.stars()} stars`,
+      }));
+
+    it('should be', () => {
+      expect(store.get.title('Repository: ')).toBe(
+        'Repository: zustood with 0 stars'
+      );
+    });
+  });
+
+  describe('when set.state', () => {
+    const store = createStore('repo')({
+      name: 'zustood',
+      stars: 0,
+    });
+
+    it('should be', () => {
+      store.set.state((draft) => {
+        draft.name = 'test';
+        draft.stars = 1;
+      });
+
+      expect(store.get.state()).toEqual({
+        name: 'test',
+        stars: 1,
+      });
+    });
   });
 });
