c1376367bcde00a06e0564e302d39e5fe78214df
ShinyLeee <lshinylee007@gmail.com>
2022-04-23 21:44:12 +0800

    fix: fix selectors always cause re-render because of always return a new function

--- packages/zustood/src/utils/extendSelectors.ts
+++ packages/zustood/src/utils/extendSelectors.ts
@@ -38,18 +38,30 @@ export const extendSelectors = <
   Object.keys(builder(api.store.getState(), api.get, api)).forEach((key) => {
     // @ts-ignore
     use[key] = (...args: any[]) =>
-      api.useStore((state) => builder(state, api.get, api)[key])(...args);
+      api.useStore((state) => {
+        const selectors = builder(state, api.get, api);
+        const selector = selectors[key];
+        return selector(...args);
+      });
     // @ts-ignore
-    useTracked[key] = (...args: any[]) =>
-      api.useStore((state) => builder(state, api.get, api)[key])(...args);
+    useTracked[key] = (...args: any[]) => {
+      const trackedState = api.useTrackedStore();
+      const selectors = builder(trackedState, api.get, api);
+      const selector = selectors[key];
+      return selector(...args);
+    };
     // @ts-ignore
-    get[key] = (...args: any[]) =>
-      builder(api.store.getState(), api.get, api)[key](...args);
+    get[key] = (...args: any[]) => {
+      const selectors = builder(api.store.getState(), api.get, api);
+      const selector = selectors[key];
+      return selector(...args);
+    };
   });
 
   return {
     ...(api as any),
     get,
     use,
+    useTracked,
   };
 };
