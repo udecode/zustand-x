da3a40346cb8340d60bbac2182f4ca7d749abc00
zbeyens <zbeyens@udecode.dev>
2025-02-04 23:24:58 +0100

    vitest

--- config/eslint/bases/vitest.cjs
+++ config/eslint/bases/vitest.cjs
@@ -2,17 +2,21 @@ const vitest = require('@vitest/eslint-plugin');
 
 module.exports = {
   plugins: ['vitest'],
-  rules: {
-    ...vitest.configs.recommended.rules,
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-empty-function': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-object-literal-type-assertion': 'off',
-    'import/default': 'off',
-    'import/namespace': 'off',
-    'import/no-duplicates': 'off',
-    'import/no-named-as-default-member': 'off',
-  },
-  files: ['**/?(*.)+(test|spec).{js,jsx,ts,tsx}'],
+  overrides: [
+    {
+      files: ['**/?(*.)+(test|spec).{js,jsx,ts,tsx}'],
+      rules: {
+        ...vitest.configs.recommended.rules,
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-object-literal-type-assertion': 'off',
+        'import/default': 'off',
+        'import/namespace': 'off',
+        'import/no-duplicates': 'off',
+        'import/no-named-as-default-member': 'off',
+      },
+    },
+  ],
 };
