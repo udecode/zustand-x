f5801e9bdbbe7e062e778b8a28543f8866fede94
zbeyens <zbeyens@udecode.dev>
2025-02-05 09:56:19 +0100

    fix

---
+++ .changeset/rich-bikes-talk.md
@@ -0,0 +1,5 @@
+---
+'zustand-x': patch
+---
+
+Fix exports
--- packages/zustand-x/src/index.ts
+++ packages/zustand-x/src/index.ts
@@ -3,6 +3,7 @@
  */
 
 export * from './createStore';
+export * from './useStore';
 export * from './middlewares/index';
 export * from './types/index';
 export * from './utils/index';
